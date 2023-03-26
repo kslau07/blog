8ac578b439f82f4d968c052c78d31d9cbf0df291
sam-dell <cwm9889@gmail.com>
2023-03-26 12:00:24 -0400

    feat: finish 7.4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"          # route to show just 1 article  
+  resources :articles
+
+  #get "/articles", to: "articles#index"
+  #get "/articles/:id", to: "articles#show"          # route to show just 1 article  
 end
