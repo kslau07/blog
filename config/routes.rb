5cd76a4ec27ac16f0ea4e5108301f4a432b37e7e
sam <abluecar@gmail.com>
2023-03-27 11:34:52 -0400

    finish section 9.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
   #get "/articles", to: "articles#index"
   #get "/articles/:id", to: "articles#show"          # route to show just 1 article  
